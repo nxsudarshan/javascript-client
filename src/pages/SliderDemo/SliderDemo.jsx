b16f8ebf42c603da828b4db1e9ba271c0b4186e5
sudarshan <sudarshan.gonewar@successive.tech>
2019-05-21 19:42:59 +0530

    feat : add lint and husky update in component width

---
+++ src/pages/SliderDemo/SliderDemo.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/require-render-return */
+/* eslint-disable no-useless-constructor */
+import React from 'react';
+import { Slider } from '../../components/index';
+
+export class SliderDemo extends React.Component {
+  images = [
+    'cloud.jpg',
+    'dns-server.png',
+    'full-stack-web-development.jpg',
+    'js.jpg',
+    'load-balancer.png',
+  ];
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Slider
+        altText="Default Banner"
+        banners={this.images}
+        defaultBanner="default.png"
+        duration="2000"
+        height="200"
+        random
+      />
+    );
+  }
+}
